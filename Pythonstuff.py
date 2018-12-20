f64944927e512391b42f6ceb72dfed3dde546dd7
ii187 <lulzim.selimi@gmail.com>
2018-12-20 12:22:14 +0100

    ,,,,

---
+++ Pythonstuff.py
@@ -0,0 +1,96 @@
+# Temperatur umrechnen
+
+"""
+temp = eval(input("Enter a Temparature in Celsius:"))
+f_temp = 9/5*temp+32
+print("In Fahrenheit,that is ", 9/5*temp+32)
+if f_temp > 212:
+    print("Es kocht")
+if f_temp <= 32:
+    print("Arschkalt")
+
+
+x = 512-282
+y = (47*48)+5
+
+print(x/y)
+
+
+
+number = eval(input("Geben sie eine Nummer ein"))
+print("the square of is", int(number*number))
+"""
+# Durschnittberechnung
+"""
+num1 = eval(input("Enter the first number: "))
+num2 = eval(input("Enter the second number: "))
+print("The average of the numbers you entered is", (num1+num2)/2)
+
+
+name = input("Geben sie ihren Namen ein: ")
+print("hallo, " ,name)
+"""
+
+
+"""
+num = eval(input("Enter a Number"))
+print("Your number squared", num*num)
+
+
+kg = eval(input("Enter your KG"))
+print("Your KG are in Pounds", kg*2.2)
+"""
+# Eingabe 3 mal anschliessende Ausgabe von Durchschnitt und Totaleingabe
+"""
+num1 = eval(input("Bitte geben sie ihre erste nr ein"))
+num2 = eval(input("Bitte geben sie ihre zweite nr ein"))
+num3 = eval(input("Bitte geben sie ihre dritte nr ein"))
+
+total = num1+num2+num3
+durchschnitt = total/3
+
+print("Total",total, "Durchschnitt",durchschnitt)
+"""
+"""
+Beleg = 20
+print("Bitte bezahlen sie:", Beleg)
+tip = eval(input("Trinkgeld?"))
+Endsumme = Beleg+tip
+print("Bitte bezahlen sie:", + Endsumme)
+
+
+Hello = 2
+for i in range(10):
+    print(Hello)
+    print("Hello")
+
+
+# Dreimalige Abfrage und quadrieren
+
+for i in range(3):
+    num = eval(input("Enter a number:"))
+    print("The square of your number is", num*num)
+print("The loop is done now")
+"""
+
+
+"""
+for i in range(5,0,-1):
+    print(i, end="")
+print("Action")
+"""
+# 1 Luli 2 Luli 3 Luli 4 Luli .... 100 luli
+"""
+for i in range(0,100):
+    print(i+1, "Luli", "", end="")
+"""
+
+
+# 1 --- 1
+# 2 --- 4
+# 3 --- 9
+# 20 --- 400
+""" 
+for i in range(21):
+    print(i, "---", i*i)
+"""
